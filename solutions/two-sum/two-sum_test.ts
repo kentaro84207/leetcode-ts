7c64827418b2b9d7f55a2821b11eb7ae40b31d7f
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-05-16 11:03:25 +0900

    Minor fix (#4)

--- solutions/two-sum/two-sum_test.ts
+++ solutions/two-sum/two-sum_test.ts
@@ -1,7 +1,7 @@
-import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
-import { twoSum } from './two-sum.ts';
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { twoSum } from "./two-sum.ts";
 
-Deno.test('1. Two Sum', () => {
+Deno.test("1. Two Sum", () => {
   assertEquals(twoSum([2, 7, 11, 15], 9), [0, 1]);
   assertEquals(twoSum([2, 7, 11, 15], 22), [1, 3]);
 });
--- solutions/valid-parentheses/valid-parentheses.ts
+++ solutions/valid-parentheses/valid-parentheses.ts
@@ -1,17 +1,17 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 export const isValid = (s: string): boolean => {
-  if (s.length === 0) return true
+  if (s.length === 0) return true;
 
-  const sArr = s.split('')
-  const stackArr: string[] = []
+  const sArr = s.split("");
+  const stackArr: string[] = [];
 
   for (let i = 0; i < sArr.length; i++) {
-    const c = sArr[i]
-    if (c === '(') stackArr.push(')')
-    else if (c === '{') stackArr.push('}')
-    else if (c === '[') stackArr.push(']')
-    else if (stackArr.length === 0 || c !== stackArr.pop()) return false
+    const c = sArr[i];
+    if (c === "(") stackArr.push(")");
+    else if (c === "{") stackArr.push("}");
+    else if (c === "[") stackArr.push("]");
+    else if (stackArr.length === 0 || c !== stackArr.pop()) return false;
   }
-  return stackArr.length === 0
-}
+  return stackArr.length === 0;
+};
