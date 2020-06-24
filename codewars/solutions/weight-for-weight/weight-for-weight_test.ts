83c6e25344d97a142d5d03b08b6382d0c07ba3be
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-06-24 16:17:50 +0900

    Weight for weight (#21)

---
+++ codewars/solutions/weight-for-weight/weight-for-weight_test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { orderWeight } from "./weight-for-weight.ts";
+
+Deno.test("Weight for weight", () => {
+  assertEquals(orderWeight("103 123 4444 99 2000"), "2000 103 123 4444 99");
+  assertEquals(
+    orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"),
+    "11 11 2000 10003 22 123 1234000 44444444 9999",
+  );
+});
