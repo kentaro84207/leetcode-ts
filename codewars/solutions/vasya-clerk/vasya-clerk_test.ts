da8b2ae6868b4934aa3a4f9c14b4991517a51258
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-06-29 18:47:36 +0900

    Vasya-Clerk (#23)

---
+++ codewars/solutions/vasya-clerk/vasya-clerk_test.ts
@@ -0,0 +1,42 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { tickets } from "./vasya-clerk.ts";
+
+Deno.test("Vasya-Clerk", () => {
+  assertEquals(tickets([25, 25, 50, 50]), "YES");
+  assertEquals(tickets([25, 100]), "NO");
+  assertEquals(
+    tickets([25, 25, 25, 100, 25, 25, 50, 100, 25, 25, 50, 100, 100, 100]),
+    "NO",
+  );
+  assertEquals(
+    tickets(
+      [
+        25,
+        50,
+        25,
+        100,
+        25,
+        25,
+        25,
+        100,
+        25,
+        25,
+        25,
+        100,
+        25,
+        25,
+        25,
+        100,
+        25,
+        50,
+        25,
+        100,
+      ],
+    ),
+    "YES",
+  );
+  assertEquals(
+    tickets([25, 50, 25, 50, 100, 25, 25, 50]),
+    "NO",
+  );
+});
