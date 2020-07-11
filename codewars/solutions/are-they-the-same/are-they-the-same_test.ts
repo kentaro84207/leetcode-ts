abc51a25533b02c8639824b7b8a7f27a9df7ad42
KentaroSuzuki <kentaro84207@gmail.com>
2020-07-11 11:24:13 +0900

    Are they the same?

---
+++ codewars/solutions/are-they-the-same/are-they-the-same_test.ts
@@ -0,0 +1,29 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { comp } from "./are-they-the-same.ts";
+
+Deno.test("Are they the same?", () => {
+  assertEquals(
+    comp(
+      [121, 144, 19, 161, 19, 144, 19, 11],
+      [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+    ),
+  true);
+  assertEquals(
+    comp(
+      [],
+      [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+    ),
+  false);
+  assertEquals(
+    comp(
+      [121, 144, 19, 161, 19, 144, 19, 11],
+      []
+    ),
+  false);
+  assertEquals(
+    comp(
+      [ 2, 2, 3 ],
+      [ 4, 9, 9 ]
+    ),
+  false);
+}); 
