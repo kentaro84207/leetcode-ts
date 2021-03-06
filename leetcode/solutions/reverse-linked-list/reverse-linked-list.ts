f7dc75fcd57770414d7834a39ed8acd420dd02bf
Kentaro Suzuki <kentaro84207@gmail.com>
2021-03-06 16:06:08 +0900

    Reverse Linked List - Stack

--- leetcode/solutions/reverse-linked-list/reverse-linked-list.ts
+++ leetcode/solutions/reverse-linked-list/reverse-linked-list.ts
@@ -1,8 +1,9 @@
 // https://leetcode.com/problems/reverse-linked-list/
-// Iterative
 import ListNode from '../../../datastructure/LinkedList/ListNode.ts';
 
-export const reverseList = (head: ListNode<number> | null): ListNode<number> | null => {
+export const reverseListIterative = (
+  head: ListNode<number> | null
+): ListNode<number> | null => {
   let prevNode: ListNode<number> | null = null;
   let headNode: ListNode<number> | null = head;
   while (headNode !== null) {
@@ -12,4 +13,23 @@ export const reverseList = (head: ListNode<number> | null): ListNode<number> | n
     headNode = nextNode;
   }
   return prevNode;
+};
+
+export const reverseListStack = (
+  head: ListNode<number> | null
+): ListNode<number> | null => {
+  if (head === null) return null;
+  const stack = [];
+  while (head !== null) {
+    stack.push(head);
+    head = head.next;
+  }
+  let current = stack.pop()!;
+  head = current;
+  while (stack.length > 0) {
+    current.next = stack.pop()!;
+    current = current.next;
+  }
+  current.next = null;
+  return head;
 };
