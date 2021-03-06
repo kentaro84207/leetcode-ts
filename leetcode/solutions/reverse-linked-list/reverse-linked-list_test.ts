b0bf57bcc15a1f38fb2be6d9e511d3c1c3fe0727
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2021-03-06 16:06:39 +0900

    Reverse Linked List - Stack (#46)

--- leetcode/solutions/reverse-linked-list/reverse-linked-list_test.ts
+++ leetcode/solutions/reverse-linked-list/reverse-linked-list_test.ts
@@ -1,12 +1,24 @@
 import { assertEquals } from 'https://deno.land/std@0.60.0/testing/asserts.ts';
-import { reverseList } from './reverse-linked-list.ts';
+import { reverseListIterative, reverseListStack } from './reverse-linked-list.ts';
 import { CreateListNode } from '../../../datastructure/LinkedList/ListNode.ts';
 
-Deno.test('206. Reverse Linked List', () => {
+Deno.test('206. Reverse Linked List - Iterative', () => {
   assertEquals(
-    reverseList(CreateListNode([1, 2, 3, 4, 5])),
+    reverseListIterative(CreateListNode([1, 2, 3, 4, 5])),
     CreateListNode([5, 4, 3, 2, 1])
   );
-  assertEquals(reverseList(CreateListNode([1, 2])), CreateListNode([2, 1]));
-  assertEquals(reverseList(CreateListNode([])), CreateListNode([]));
+  assertEquals(
+    reverseListIterative(CreateListNode([1, 2])),
+    CreateListNode([2, 1])
+  );
+  assertEquals(reverseListIterative(CreateListNode([])), CreateListNode([]));
+});
+
+Deno.test('206. Reverse Linked List - Stack', () => {
+  assertEquals(
+    reverseListStack(CreateListNode([1, 2, 3, 4, 5])),
+    CreateListNode([5, 4, 3, 2, 1])
+  );
+  assertEquals(reverseListStack(CreateListNode([1, 2])), CreateListNode([2, 1]));
+  assertEquals(reverseListStack(CreateListNode([])), CreateListNode([]));
 });
