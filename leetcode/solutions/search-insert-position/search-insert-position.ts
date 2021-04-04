f483a0f78ecb1727ab2586360a99dc13c018f112
Kentaro Suzuki <kentaro84207@gmail.com>
2021-04-04 12:39:54 +0900

    Search Insert Position - BinarySearch

--- leetcode/solutions/search-insert-position/search-insert-position.ts
+++ leetcode/solutions/search-insert-position/search-insert-position.ts
@@ -1,9 +1,6 @@
 // https://leetcode.com/problems/search-insert-position/
 
-export const searchInsert = (
-  nums: number[],
-  target: number,
-): number => {
+export const searchInsert = (nums: number[], target: number): number => {
   const numExists = nums.indexOf(target);
   if (numExists !== -1) return numExists;
 
@@ -17,4 +14,25 @@ export const searchInsert = (
   }
 
   return output;
+};
+
+export const searchInsertBinarySearch = (
+  nums: number[],
+  target: number
+): number => {
+  let left = 0;
+  let right = nums.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  while (left <= right) {
+    if (target === nums[middle]) return middle;
+    if (target > nums[middle]) {
+      left = middle + 1;
+    } else if (target < nums[middle]) {
+      right = middle - 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+
+  return left;
 };
--- leetcode/solutions/search-insert-position/search-insert-position_test.ts
+++ leetcode/solutions/search-insert-position/search-insert-position_test.ts
@@ -1,9 +1,16 @@
 import { assertEquals } from "https://deno.land/std@0.60.0/testing/asserts.ts";
-import { searchInsert } from "./search-insert-position.ts";
+import { searchInsert, searchInsertBinarySearch } from "./search-insert-position.ts";
 
 Deno.test("35. Search Insert Position", () => {
   assertEquals(searchInsert([1, 3, 5, 6], 5), 2);
   assertEquals(searchInsert([1, 3, 5, 6], 2), 1);
   assertEquals(searchInsert([1, 3, 5, 6], 7), 4);
   assertEquals(searchInsert([1, 3, 5, 6], 0), 0);
+});
+
+Deno.test("35. Search Insert Position - BinarySearch", () => {
+  assertEquals(searchInsertBinarySearch([1, 3, 5, 6], 5), 2);
+  assertEquals(searchInsertBinarySearch([1, 3, 5, 6], 2), 1);
+  assertEquals(searchInsertBinarySearch([1, 3, 5, 6], 7), 4);
+  assertEquals(searchInsertBinarySearch([1, 3, 5, 6], 0), 0);
 });
